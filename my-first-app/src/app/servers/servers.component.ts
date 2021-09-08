088214b2ab6ab13259976e72780263a0df66641c
Ron Schmitt <ron.schmitt@protonmail.com>
2021-09-08 18:46:21 -0400

    thru chap 30

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '.app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
-  // templateUrl: './servers.component.html',
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus ='No server was created';
+  constructor() {
+    setTimeout( ()=> {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
 }
