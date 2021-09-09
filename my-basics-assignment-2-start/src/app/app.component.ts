4beaf6dadafd8a901bc083a309804fd54e7ee989
Ron Schmitt <ron.schmitt@protonmail.com>
2021-09-08 20:16:04 -0400

    thru chap 38

---
+++ my-basics-assignment-2-start/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  userName='';
+  proceedStatus = 'waiting for click';
+
+  constructor() {
+
+  }
+
+  onProceed() {
+    this.proceedStatus = 'proceed to login';
+  }
+}
