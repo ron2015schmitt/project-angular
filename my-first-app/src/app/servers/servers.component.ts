cc877f73af058c5d1e6afcff2513d4d04d587ef1
Ron Schmitt <ron.schmitt@protonmail.com>
2021-09-12 19:57:25 -0400

    thru chap 40

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreated = false;
   serverName = 'TestServer';
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() {
     setTimeout( ()=> {
@@ -21,6 +22,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
   }
 
   onUpdateServerName(event: Event) {
