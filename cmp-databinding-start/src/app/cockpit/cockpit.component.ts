e38d5c01d9fc7ed2c9b319029a212beb2521c19a
Ron Schmitt <ron.schmitt@protonmail.com>
2021-09-17 14:20:28 -0400

    thru chap 75

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,22 +8,25 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class CockpitComponent  {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+
+  // { static: true } needed so that it can be accessed inside ngOnInit
+  @ViewChild('serverContentInput', { static: true } ) serverContentInput: ElementRef;
   // newServerName: string = 'MyName';
-  newServerContent: string = 'MyContent';
+  // newServerContent: string = 'MyContent';
 
 
   onAddServer(nameInput: HTMLInputElement) {
     // console.log(`onAddServer; nameInput=${nameInput.value} newServerName=${this.newServerName} newServerContent=${this.newServerContent} `);
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 }
