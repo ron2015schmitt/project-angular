7b409906601b69b22223dbeb4b6233d8620b18d1
Ron Schmitt <ron.schmitt@protonmail.com>
2021-09-16 20:39:11 -0400

    thru chap 56

---
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit {
+
+  element = {
+    name: '',
+    type: '',
+    content: '',
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
