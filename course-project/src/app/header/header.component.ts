a8163de7a987bdeb9da79407487a0402494088a6
Ron Schmitt <ron.schmitt@protonmail.com>
2021-09-23 15:09:08 -0400

    thru chap 85

--- course-project/src/app/header/header.component.ts
+++ course-project/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
@@ -7,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
+  @Output() selectedFeature = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(feature: string) {
+    this.selectedFeature.emit(feature);
   }
 
 }
